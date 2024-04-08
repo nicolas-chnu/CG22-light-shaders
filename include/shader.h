98189637beb53be4d1e5035298a093d4941beb4c
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2024-04-08 21:36:28 +0300

    Add light switching flag

--- include/shader.h
+++ include/shader.h
@@ -5,6 +5,10 @@
 #include "raylib.h"
 #endif
 
+#ifndef RAYMATH_H
+#include "raymath.h"
+#endif
+
 typedef struct MyMaterial {
     Vector3 ambient;
     Vector3 diffuse;
@@ -13,7 +17,7 @@ typedef struct MyMaterial {
 } MyMaterial;
 
 typedef struct MyLight {
-    bool enabled;
+    int enabled;
     Vector3 position;
 
     Vector3 ambient;
@@ -22,12 +26,47 @@ typedef struct MyLight {
 } MyLight;
 
 MyMaterial CreateBronze() {
-    return (MyMaterial){
+    return (MyMaterial) {
         .ambient = (Vector3){0.2125, 0.1275, 0.054},
         .diffuse = (Vector3){0.714, 0.4284, 0.18144},
         .specular = (Vector3){0.393548, 0.271906, 0.166721},
-        .shininess = 0.2
+        .shininess = 72    // 0.2
     };
+}
+
+MyMaterial CreatePearl() {
+    return (MyMaterial) {
+        .ambient = (Vector3){0.25, 0.20725, 0.20725},
+        .diffuse = (Vector3){1, 0.829, 0.829},
+        .specular = (Vector3){0.296648, 0.296648, 0.296648},
+        .shininess = 0.088    // 0.088
+    };
+}
+
+MyMaterial CreateGreenRubber() {
+    return (MyMaterial) {
+        .ambient = (Vector3){0.0, 0.05, 0.0},
+        .diffuse = (Vector3){0.4, 0.5, 0.4},
+        .specular = (Vector3){0.04,	0.7, 0.04},
+        .shininess = 0.5    // 0.078125
+    };
+}
+
+void UpdateLightColor(MyLight* li) {
+    float tk = sinf((float)GetTime());
+
+    #ifdef SWITCH_LIGHTS
+    Vector3 lightColor = {tk * 2.0, tk * 0.7, tk * 1.3};
+    #else
+    Vector3 lightColor = {2.0, 0.7, 1.3};
+    #endif
+    
+    Vector3 diffuseColor = Vector3Multiply(lightColor, (Vector3){.5, .5, .5});
+    Vector3 ambientColor = Vector3Multiply(diffuseColor, (Vector3){.2, .2, .2});
+
+    (*li).ambient = ambientColor;
+    (*li).diffuse = diffuseColor;
+    (*li).specular = (Vector3){1, 1, 1};
 }
 
 void UpdateMaterialShader(Shader sh, MyMaterial mat, MyLight li, Vector3 viewPos) {
--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include "raylib.h"
+#include "rlgl.h"
 #include "shader.h"
 
+// #define SWITCH_LIGHTS       // uncomment to enable light switching 
 #define TILE_LENGTH 4.0f
 
 Vector3 PosFromTiles(float x, float y, float z);
+void UpdateLightEnabled(int *enabled);
+void UpdateLightPos(Vector3 *pos);
 
 int main(void)
 {
@@ -19,21 +23,70 @@ int main(void)
         .fovy = 45.0f,
         .projection = CAMERA_PERSPECTIVE
     };
-    
+
+    // light
+    MyLight light = {0};
+
+    // shaders
+    Shader cube1Shader = LoadShader("shaders/mat.vs", "shaders/mat.fs");
+    Shader cube2Shader = LoadShader("shaders/mat.vs", "shaders/mat.fs");
+    Shader planeShader = LoadShader("shaders/mat.vs", "shaders/mat.fs");
+    Shader lightSourceShader = LoadShader("shaders/light.vs", "shaders/light.fs");
+
+    // models
+    Model cube1 = LoadModelFromMesh(GenMeshCube(TILE_LENGTH, TILE_LENGTH, TILE_LENGTH));
+    cube1.materials[0].shader = cube1Shader;
+
+    Model cube2 = LoadModelFromMesh(GenMeshCube(TILE_LENGTH, TILE_LENGTH, TILE_LENGTH));
+    cube2.materials[0].shader = cube2Shader;
+
+    Model plane = LoadModelFromMesh(GenMeshPlane(TILE_LENGTH * 40, TILE_LENGTH * 40, 1, 1));
+    plane.materials[0].shader = planeShader;
+
+    Model sphere = LoadModelFromMesh(GenMeshSphere(TILE_LENGTH / 4, 32, 32));
+    sphere.materials[0].shader = lightSourceShader;
+
+    // materials
+    MyMaterial bronze = CreateBronze();
+    MyMaterial greenRubber = CreateGreenRubber();
+    MyMaterial pearl = CreatePearl();
+
     SetTargetFPS(60); 
 
     while (!WindowShouldClose())   
     {
         UpdateCamera(&camera, CAMERA_ORBITAL);
 
+        UpdateLightPos(&light.position);
+        UpdateLightEnabled(&light.enabled);
+        UpdateLightColor(&light);
+
+        UpdateMaterialShader(planeShader, greenRubber, light, camera.position);
+        UpdateMaterialShader(cube1Shader, bronze, light, camera.position);
+        UpdateMaterialShader(cube2Shader, pearl, light, camera.position);
+
         BeginDrawing();
             ClearBackground(RAYWHITE);
-
             BeginMode3D(camera);
                 
+                DrawModel(plane, PosFromTiles(0, 0, 0), 1, WHITE);
+                DrawModel(cube1, PosFromTiles(0, 0.5, 0), 1, WHITE);
+                DrawModel(cube2, PosFromTiles(2, 0.5, 2), 1, WHITE);
+
+                DrawModel(sphere, light.position, 1, light.enabled == 1 ? WHITE : BLACK);
             EndMode3D();
         EndDrawing();
     }
+
+    UnloadModel(plane);
+    UnloadModel(cube1);
+    UnloadModel(cube2);
+    UnloadModel(sphere);
+
+    UnloadShader(planeShader);
+    UnloadShader(cube1Shader);
+    UnloadShader(cube2Shader);
+    UnloadShader(lightSourceShader);
 
     CloseWindow();
     return 0;
@@ -41,4 +94,28 @@ int main(void)
 
 Vector3 PosFromTiles(float x, float y, float z) {
     return (Vector3){x * TILE_LENGTH, y * TILE_LENGTH, z * TILE_LENGTH};
+}
+
+void UpdateLightEnabled(int* enabled) {
+    if (IsKeyPressed(KEY_SPACE)) {
+        (*enabled) = ((*enabled) == 0) ? 1 : 0;
+    }
+}
+
+void UpdateLightPos(Vector3 *pos) {
+    float step = 0.1;
+
+    if (IsKeyDown(KEY_I)) {
+        (*pos).x += step;
+    } else if (IsKeyDown(KEY_J)) {
+        (*pos).z -= step;
+    } else if (IsKeyDown(KEY_K)) {
+        (*pos).x -= step;
+    } else if (IsKeyDown(KEY_L)) {
+        (*pos).z += step;
+    } else if (IsKeyDown(KEY_U)) {
+        (*pos).y += step;
+    } else if (IsKeyDown(KEY_O)) {
+        (*pos).y -= step;
+    }
 }
